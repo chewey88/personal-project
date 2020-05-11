135a90034ff5d671ec71809cdd20a0d33775359b
Christopher Sovine <chewey88@gmail.com>
2020-05-11 11:18:04 -0600

    before-hosting-commit

--- server/index.js
+++ server/index.js
@@ -1,47 +1,54 @@
-require('dotenv').config()
-const express = require('express')
-const massive = require('massive')
-const session = require('express-session')
-const authCtrl = require('./controllers/authController')
-const commentCtrl = require('./controllers/commentsController')
-const trailCtrl = require('./controllers/trailController')
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const session = require("express-session");
+const authCtrl = require("./controllers/authController");
+const commentCtrl = require("./controllers/commentsController");
+const trailCtrl = require("./controllers/trailController");
 // const searchCtrl = require('./controllers/searchController')
-const{SERVER_PORT, CONNECTION_STRING,
-SESSION_SECRET} = process.env
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(
-    session({
-        resave: false,
-        saveUninitialized: true,
-        cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},
-        secret: SESSION_SECRET,
-    })
-)
+  session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+    secret: SESSION_SECRET,
+  })
+);
 
-app.post('/auth/login', authCtrl.login)
-app.post('/auth/register', authCtrl.register)
-app.delete('/auth/logout', authCtrl.logout)
-app.get('/auth/user', authCtrl.getUser)
+app.post("/auth/login", authCtrl.login);
+app.post("/auth/register", authCtrl.register);
+app.delete("/auth/logout", authCtrl.logout);
+app.get("/auth/user", authCtrl.getUser);
 
-app.get('/api/comments', commentCtrl.getComments)
-app.post('/api/comments', commentCtrl.addComment)
-app.put('/api/comments/:comment_id', commentCtrl.editComment)
-app.delete('/api/comments/:comment_id', commentCtrl.deleteComment)
+app.get("/api/comments", commentCtrl.getComments);
+app.post("/api/comments", commentCtrl.addComment);
+app.put("/api/comments/:comment_id", commentCtrl.editComment);
+app.delete("/api/comments/:comment_id", commentCtrl.deleteComment);
 
-app.get('/api/trails', trailCtrl.getTrails)
+app.get("/api/trails", trailCtrl.getTrails);
 // app.get('/api/search', searchCtrl.getMinerals)
 
+app.use(express.static(__dirname + "/../build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    },
-}).then(dbInstance => {
-    app.set('db', dbInstance)
-    console.log('db ready')
-    app.listen(SERVER_PORT, () => console.log(`Ready to collect on port ${SERVER_PORT}`))
-})
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((dbInstance) => {
+  app.set("db", dbInstance);
+  console.log("db ready");
+  app.listen(SERVER_PORT, () =>
+    console.log(`Ready to collect on port ${SERVER_PORT}`)
+  );
+});
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {HashRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './ducks/store'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./ducks/store";
+const Router =
+  process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
